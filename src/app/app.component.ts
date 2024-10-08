358d070f0bc3fd5521e6bb788253f04229d90c67
1azuli <prapawit.pl@ku.th>
2024-10-08 16:05:21 +0700

    work-1-forTae

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { UsersService } from './services/users.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'project_SA';
+  title = 'restuarant';
+  logined: boolean = false;
+
+  constructor(private usersService: UsersService, private router: Router) {
+    if (localStorage.getItem('username') !== '' && localStorage.getItem('username') !== null) {
+      this.logined = true;
+      this.router.navigate(['/admin/manage']);
+    }
+  }
+
+  logout() {
+    localStorage.setItem('username', '');
+    this.logined = false;
+  }
+
 }
